1d9a777b7e37cd8be6c38f42591efb9534c5e8a8
me <>
2024-10-11 13:14:02 +0100

    completed brute force sudoku solver solution in rust!

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
+use std::collections::HashSet;
+
 type Sudoku = [[i32; 9]; 9];
-
-
 
 fn main() {
     println!("Hello, world!");
-    let foo = legal_digits(&[1, 2, 3]);
-    println!("{:?}", foo);
-    println!(
-        "{:?}",
-        get_box_cells(
-            &[
-                [2, 9, 0, 0, 0, 0, 0, 7, 8],
-                [0, 0, 0, 0, 8, 0, 0, 0, 0],
-                [0, 0, 5, 2, 7, 0, 0, 4, 1],
-                [0, 0, 0, 9, 0, 0, 1, 0, 6],
-                [0, 0, 1, 0, 0, 0, 9, 0, 0],
-                [9, 0, 4, 0, 0, 6, 0, 0, 0],
-                [7, 6, 0, 0, 3, 8, 4, 0, 0],
-                [0, 0, 0, 0, 9, 0, 0, 0, 0],
-                [3, 1, 0, 0, 0, 0, 0, 9, 8],
-            ],
-            5
-        )
-    );
-
-    let mut _sud = [
-        [2, 9, 0, 0, 0, 0, 0, 7, 8],
+    let sud = [
+        [2, 9, 0, 0, 0, 0, 0, 8, 7],
         [0, 0, 0, 0, 8, 0, 0, 0, 0],
         [0, 0, 5, 2, 7, 0, 0, 4, 1],
         [0, 0, 0, 9, 0, 0, 1, 0, 6],
@@ -35,18 +15,69 @@ fn main() {
         [0, 0, 0, 0, 9, 0, 0, 0, 0],
         [3, 1, 0, 0, 0, 0, 0, 9, 8],
     ];
+
+    if let Some(result) = iteration(&sud) {
+        println!("result was a winner {:?}", result);
+    } else {
+        println!("oliver broke my program again");
+    }
 }
 
-fn legal_digits_for_cell(sudoku: &Sudoku, x: i32, y: i32) {
-    let col = get_col(sudoku, x);
-    let row = get_row(sudoku, y);
-    let box = get_box_for_cell(sudoku, x, y);
+fn iteration(sud: &Sudoku) -> Option<Sudoku> {
+    // find the cell with the lowest variability
+    let lowest_variability_option = sud
+        .as_flattened()
+        .into_iter()
+        .enumerate()
+        .filter(|(_, cell_value)| **cell_value == 0)
+        .map(|(idx, _)| {
+            (
+                (idx % 9, idx / 9),
+                legal_digits_for_cell(&sud, (idx % 9) as i32, (idx / 9) as i32),
+            )
+        })
+        .min_by_key(|(_, legals)| legals.len());
+
+    let Some((cell_to_try, cell_to_try_values)) = lowest_variability_option else {
+        return Some(*sud);
+    };
+
+    let mut trial_sud = sud.clone();
+    for ele in cell_to_try_values.into_iter() {
+        set_cell_at(
+            &mut trial_sud,
+            cell_to_try.0 as i32,
+            cell_to_try.1 as i32,
+            ele,
+        );
+        if let Some(result) = iteration(&trial_sud) {
+            return Some(result);
+        }
+    }
+    return None;
+}
+
+fn legal_digits_for_cell(sudoku: &Sudoku, x: i32, y: i32) -> Vec<i32> {
+    let mut relevant = get_col(sudoku, x)
+        .into_iter()
+        .chain(get_row(sudoku, y))
+        .chain(get_box_for_cell(sudoku, x, y))
+        .filter(|num| *num != 0)
+        .collect::<Vec<_>>();
+
+    relevant.sort();
+    relevant.dedup();
+
+    return legal_digits(&relevant);
 }
 
 fn legal_digits(slice: &[i32]) -> Vec<i32> {
-    let mut legals = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-    legals.retain(|el| !slice.contains(el));
-    return legals;
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        .into_iter()
+        .collect::<HashSet<i32>>()
+        .symmetric_difference(&slice.iter().cloned().collect::<HashSet<i32>>())
+        .cloned()
+        .collect::<Vec<i32>>()
 }
 
 fn get_row(sudoku: &Sudoku, row: i32) -> [i32; 9] {
@@ -54,7 +85,7 @@ fn get_row(sudoku: &Sudoku, row: i32) -> [i32; 9] {
 }
 
 fn get_col(sudoku: &Sudoku, col: i32) -> [i32; 9] {
-    let col_idx = col as usize
+    let col_idx = col as usize;
     return [
         sudoku[0][col_idx],
         sudoku[1][col_idx],
@@ -70,6 +101,10 @@ fn get_col(sudoku: &Sudoku, col: i32) -> [i32; 9] {
 
 fn get_cell_at(sudoku: &Sudoku, x: i32, y: i32) -> i32 {
     return sudoku[y as usize][x as usize];
+}
+
+fn set_cell_at(sudoku: &mut Sudoku, x: i32, y: i32, val: i32) {
+    sudoku[y as usize][x as usize] = val;
 }
 
 const BOX_OFFSETS: [(i32, i32); 9] = [
@@ -96,37 +131,37 @@ fn get_box_cells(sudoku: &Sudoku, box_idx: i32) -> Vec<i32> {
 fn get_box_for_cell(sudoku: &Sudoku, x: i32, y: i32) -> Vec<i32> {
     let mut box_idx = 0;
     if x >= 0 && x < 3 {
-        if (y >= 0 && y < 3) {
-            box_idx = 0
+        if y >= 0 && y < 3 {
+            box_idx = 0;
         }
-        if (y >= 3 && y < 6) {
-            box_idx = 3
+        if y >= 3 && y < 6 {
+            box_idx = 3;
         }
-        if (y >= 6 && y < 9) {
-            box_idx = 6
-        }
-    }
-    if (x >= 3 && x < 6) {
-        if (y >= 0 && y < 3) {
-            box_idx = 1
-        }
-        if (y >= 3 && y < 6) {
-            box_idx = 4
-        }
-        if (y >= 6 && y < 9) { 
-            box_idx = 7
+        if y >= 6 && y < 9 {
+            box_idx = 6;
         }
     }
-    if (x >= 6 && x < 9) {
-        if (y >= 0 && y < 3) {
-            box_idx = 2
+    if x >= 3 && x < 6 {
+        if y >= 0 && y < 3 {
+            box_idx = 1;
         }
-        if (y >= 3 && y < 6) {
-            box_idx = 5
+        if y >= 3 && y < 6 {
+            box_idx = 4;
         }
-        if (y >= 6 && y < 9) {
-            box_idx = 8
+        if y >= 6 && y < 9 {
+            box_idx = 7;
         }
     }
-    return get_box_cells(sudoku, box_idx)
+    if x >= 6 && x < 9 {
+        if y >= 0 && y < 3 {
+            box_idx = 2;
+        }
+        if y >= 3 && y < 6 {
+            box_idx = 5;
+        }
+        if y >= 6 && y < 9 {
+            box_idx = 8;
+        }
+    }
+    return get_box_cells(sudoku, box_idx);
 }
